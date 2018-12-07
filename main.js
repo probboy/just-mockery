3cb54bbfc525a551f2bfb78121cdaae49ccf3e81
Warren Tai <warren.w.tai@gmail.com>
2018-12-07 13:26:59 -0500

    replaced with three alternating imagines

--- main.js
+++ main.js
@@ -14,3 +14,23 @@ for (let i = 0; i < cards.length; i++) {
     "margin: 0.3em -0.7em 0.3em -0.7em;"
   );
 }
+
+const waterMelonImages = document.querySelectorAll(".card img");
+
+
+for (let i = 0; i < cards.length-2; i=i+3) {
+  waterMelonImages[i].setAttribute(
+    "src", 
+    "http://www.freshplaza.com/2018/0816/Melon.jpg"
+    );
+;
+}
+
+for (let i = 1; i < cards.length-2; i=i+3) {
+  waterMelonImages[i].setAttribute(
+    "src", 
+    "http://jpninfo.com/wp-content/uploads/2015/06/Watermelons1.jpg"
+    );
+;
+}
+
