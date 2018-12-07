168c6a1e1f6104c38fb703d57cfe61449e7af877
Warren Tai <warren.w.tai@gmail.com>
2018-12-06 19:52:07 -0500

    tidy up js

--- main.js
+++ main.js
@@ -2,13 +2,15 @@
 
 const cards = document.querySelectorAll(".card");
 
-
 const rows = document.querySelectorAll(".row");
-for(let i=0; i<rows.length; i++){
-    cards[i].setAttribute("style", "")
+
+for (let i = 0; i < rows.length; i++) {
+  cards[i].setAttribute("style", "");
 }
 
-
-for(let i=0; i<cards.length; i++){
-    cards[i].setAttribute("style", "margin-top: 0.3em; margin-bottom: 0.3em ;margin-left:-.7em; margin-right:-.7em;")
+for (let i = 0; i < cards.length; i++) {
+  cards[i].setAttribute(
+    "style",
+    "margin: 0.3em -0.7em 0.3em -0.7em;"
+  );
 }
